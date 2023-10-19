486cb6cbb8cb561841d73d7a356770e5a694150d
Cistelsa <gerenciaop@cistelsa.com>
2023-10-19 02:46:27 -0500

    Update Camilo

--- functions.py
+++ functions.py
@@ -14,22 +14,46 @@ nltk.download('stopwords')
 
 import zipfile
 
+# Ruta del archivo ZIP
+zip_file_path = '2_Datasets/beta/hotelbeds/df_hotels.zip'
+
+# Abre el archivo ZIP
+with zipfile.ZipFile(zip_file_path, 'r') as zip_file:
+    # Lista los archivos en el ZIP (puede haber múltiples archivos, no necesariamente uno)
+    zip_file_contents = zip_file.namelist()
+    
+    # Supongamos que queremos leer el primer archivo CSV en el ZIP
+    csv_file_name = zip_file_contents[0]
+    
+    # Extrae el archivo CSV del ZIP
+    with zip_file.open(csv_file_name) as csv_file:
+        # Lee el archivo CSV con Pandas
+        df_hotels_filter = pd.read_csv(csv_file)
+
 # Función para crear una lista de hoteles a través de ajax
 def hotels_list_to_html():
+
     df_hotel = pd.read_csv("2_Datasets/launch/hotels_name_to_frontend.csv")
     df_hotel = df_hotel.dropna(subset=['name'])
-    return df_hotel["name"].tolist()
+    df_state = df_hotels_filter.drop_duplicates(subset=['state_name'])
+    df_state = df_state.dropna(subset=['state_name'])
+    df_city = df_hotels_filter.drop_duplicates(subset=['city_name'])
+    df_city = df_city.dropna(subset=['city_name'])
+    df_poi = df_hotels_filter.drop_duplicates(subset=['poi_name'])
+    df_poi = df_poi.dropna(subset=['poi_name'])
+    return df_hotel["name"].tolist(), df_state["state_name"].tolist(), df_city["city_name"].tolist(), df_poi["poi_name"].tolist()
 
-    
+#función para lista filtrada estados
+def list_filter_state(estado: str):
+    city_x_state = df_hotels_filter[df_hotels_filter["state_name"] == estado]["city_name"].drop_duplicates().dropna()
+    poi_x_state = df_hotels_filter[df_hotels_filter["state_name"] == estado]["poi_name"].drop_duplicates().dropna()
+    return city_x_state.tolist(), poi_x_state.tolist()
 
-def get_recommendations_for_title(title, preview_recommendations):
-    # Cargar el DataFrame precalculado desde el archivo CSV
-    recommendations = preview_recommendations[preview_recommendations['movie_title'] == title]
-    if not recommendations.empty:
-        recommended_titles = recommendations.iloc[0]['recommended_titles']
-        return recommended_titles.split(', ')
-    else:
-        return ["Película no encontrada en la Base de Datos."]
+#función para lista filtrada ciudades
+def list_filter_city(ciudad: str):
+    poi_x_city =  df_hotels_filter[df_hotels_filter["city_name"] == ciudad]["poi_name"].drop_duplicates().dropna()
+    return poi_x_city.tolist()
+
 
 def get_sentiments_count(title, preview_recommendations):
 
@@ -112,3 +136,27 @@ def cargar_datos_y_generar_recomendaciones():
 
 # Cargar datos y generar la función de recomendación
 recommendation_function = cargar_datos_y_generar_recomendaciones()
+
+def filters(state:str = '0', 
+            city:str = '0',
+            name_point:str = '0'):
+    
+
+    try:
+        
+        if state != '0':
+            filter_state = df_hotels_filter[df_hotels_filter['state_name'] == state].sort_values(ascending=False, by=['stars'])
+            if city != '0':
+                filter_state = filter_state[filter_state['city_name'] == city].sort_values(ascending=False, by=['stars'])
+                if name_point != '0':
+                    filter_state = filter_state[filter_state['poi_name'] == name_point].sort_values(ascending=False, by=['stars'])
+            return filter_state.iloc[:3,[11,12,1,15,2,4,5,8,9,13,14]].to_html(index=False, justify="center")
+        elif city != '0':
+            filter_state = df_hotels_filter[df_hotels_filter['city_name'] == city].sort_values(ascending=False, by=['stars'])
+            return filter_state.iloc[:3,[11,12,1,15,2,4,5,8,9,13,14]].to_html(index=False, justify="center")
+        elif name_point != '0':
+            filter_state = df_hotels_filter[df_hotels_filter['poi_name'] == name_point].sort_values(ascending=False, by=['stars'])
+            return filter_state.iloc[:3,[11,12,1,15,2,4,5,8,9,13,14]].to_html(index=False, justify="center")        
+
+    except:
+        return 'INGRESAR UNA BÚSQUEDA'
--- main.py
+++ main.py
@@ -18,13 +18,13 @@ templates = Jinja2Templates(directory="public/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
-    selected_data_hotels =  f.hotels_list_to_html()
-    context = {"request": request, "selected_data_hotels":selected_data_hotels}
+async def read_root(request: Request):
+    selected_data_hotels, selected_data_state, selected_data_city, selected_data_poi =  f.hotels_list_to_html()
+    context = {"request": request, "selected_data_hotels":selected_data_hotels, "selected_data_state":selected_data_state, "selected_data_city":selected_data_city, "selected_data_poi":selected_data_poi}
     return templates.TemplateResponse("index.html", context)
 
 @app.get("/recomendacion_1")
-def recomendacion_1( titulo: str ):
+async def recomendacion_1( titulo: str ):
     # Ruta del archivo ZIP
     zip_file_path = '2_Datasets/launch/sentiments_to_frontend.zip'
 
@@ -44,7 +44,27 @@ def recomendacion_1( titulo: str ):
     return JSONResponse(content=recomendacion_1)
 
 @app.get("/recomendacion_2")
-def recomendacion_2( titulo_2: str ):
+async def recomendacion_2( titulo_2: str ):
     recomendacion_2 = f.recommendation_function(titulo_2)
     response_data = {"recomendacion_2":recomendacion_2}
     return JSONResponse(content=response_data)
+
+@app.get("/busca_hotel")
+async def busca_hotel( estado: str, ciudad: str, poi: str ):
+    busca_hotel = f.filters(estado, ciudad, poi)
+    response_data = {"busca_hotel":busca_hotel}
+    return JSONResponse(content=response_data)
+
+@app.get("/obtener_ciudades")
+async def obtener_ciudades(estado: str):
+    # Filtrar ciudades basadas en el estado seleccionado
+    ciudades_filtradas, poi_filtrados  = f.list_filter_state(estado)
+    response_data = {"ciudades": ciudades_filtradas, "poi": poi_filtrados}
+    return response_data
+
+@app.get("/obtener_poi")
+async def obtener_poi(ciudad: str):
+    # Filtrar puntos de interés basados en la ciudad seleccionada
+    poi_filtrados  = f.list_filter_city(ciudad)
+    response_data = {"poi": poi_filtrados}
+    return response_data
